55704d6c0aa646b6fab75cc204b40e8a9cd8bc98
Jose Ruiz <jotadram6@gmail.com>
2017-03-01 14:19:32 -0600

    Final version of model splitter script

--- BSM3G_TNT_Maker/python/ModelSplitter.py
+++ BSM3G_TNT_Maker/python/ModelSplitter.py
@@ -13,47 +13,30 @@ MainChain.Add("root://cmsxrootd.fnal.gov//store/user/jruizalv/VBF-C1N2_leptonicD
 MainChain.Add("root://cmsxrootd.fnal.gov//store/user/jruizalv/VBF-C1N2_leptonicDecays_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/VBF_SUSY_signal/170227_004332/0000/OutTreeModelInfo_8.root")
 MainChain.Add("root://cmsxrootd.fnal.gov//store/user/jruizalv/VBF-C1N2_leptonicDecays_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/VBF_SUSY_signal/170227_004332/0000/OutTreeModelInfo_9.root")
 MainChain.Add("root://cmsxrootd.fnal.gov//store/user/jruizalv/VBF-C1N2_leptonicDecays_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/VBF_SUSY_signal/170227_004332/0000/OutTreeModelInfo_10.root")
-#MainChain.Add("/home/jose/VBF_DM_ROOT_FILES/OutTreeModelInfo.root")
 MainChain.LoadTree(0)
 #MainChain.Print("p")
 ##########################
-#Creating skimmed tree
-##########################
-SkimmedTree = MainChain.GetTree().CloneTree(0)
-#SkimmedTree.Print("p")
-MainChain.CopyAddresses(SkimmedTree)
-##########################
 #Geetting models histogram
 ##########################
-#MainChain.Draw("configmodel.c_str() >> SignalModels","")
-#ModelsHisto=ROOT.gDirectory.Get("SignalModels")
+MainChain.Draw("configmodel.c_str() >> SignalModels","")
+ModelsHisto=ROOT.gDirectory.Get("SignalModels")
 #ModelsHisto.Draw("")
-#TotalBins=ModelsHisto.GetNbinsX()
-#print "Total of models:", TotalBins
-#BASEOUTPUT="root://cmsxrootd.fnal.gov//store/user/jruizalv/VBF-C1N2_leptonicDecays_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/VBF_SUSY_signal/170227_004332/0000/"
-BASEOUTPUT=""
-NumberOfEntries=MainChain.GetEntries()
-SkimmedTree.Reset()
-for j in xrange(NumberOfEntries):
-    MainChain.GetEntry(j)
-    if j==0: 
-        print MainChain.configmodel.c_str()
-        CurrentModel = MainChain.configmodel.c_str()
-        output = ROOT.TFile.Open(BASEOUTPUT+CurrentModel+".root","RECREATE")
-        output.mkdir("TNT"); output.cd("TNT")
-    if CurrentModel==MainChain.configmodel.c_str():
-        #print "Same model:", MainChain.configmodel.c_str()
-        SkimmedTree.Fill()
-    else:
-        print MainChain.configmodel.c_str()
-        SkimmedTree.Write()
-        output.Close()
-        SkimmedTree.Reset()
-        CurrentModel = MainChain.configmodel.c_str()
-        output = ROOT.TFile.Open(BASEOUTPUT+CurrentModel+".root","RECREATE")
-        output.mkdir("TNT"); output.cd("TNT")
-        print "Next model:", MainChain.configmodel.c_str()
-        SkimmedTree.Fill()
-
-SkimmedTree.Write()
-output.Close()
+NumberOfModels=ModelsHisto.GetNbinsX()
+print "Total of models:", NumberOfModels
+Models=[]
+print "Entries per bin"
+for i in xrange(NumberOfModels):
+    Models.append(ModelsHisto.GetXaxis().GetBinLabel(i+1))
+    print ModelsHisto.GetXaxis().GetBinLabel(i+1), ModelsHisto.GetBinContent(i+1)
+print Models
+del(ModelsHisto)
+for i in Models:
+    #print "Processing ... "+i
+    #if Models.index(i)<90 or Models.index(i)>=95: continue
+    SkimmedTree = MainChain.CopyTree('strstr(configmodel.c_str(),\"'+i+'\")')
+    output = ROOT.TFile.Open(i+".root","RECREATE")
+    output.mkdir("TNT"); output.cd("TNT")
+    SkimmedTree.Write()
+    print "Total entries for ", i, SkimmedTree.GetEntries() 
+    output.Close()
+    del(SkimmedTree); del(output)
